afbc563e8cfb627b5377442a0274f739c81f999a
mmf09 <mmf09@YPC>
2022-10-18 21:01:00 -0700

    Add project files.

---
+++ Loop assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Loop_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            do
+            {
+                Console.WriteLine(x);
+                x++;
+                
+            }
+            while (x < 15);
+
+
+            int y = 16;
+            while (y < 20)
+            {
+                Console.WriteLine(y);
+                y++;
+
+
+
+          }
+        }
+       }
+    }
